52ad5af512db112f2d99b0717162a433c59555ea
Tanya <Tatiana.Fomina@emc.com>
2020-05-24 20:59:37 +0300

    Update attachment

--- listings/parse-object-member.js
+++ listings/parse-object-member.js
@@ -7,26 +7,10 @@ export default class ExpressionParser extends LValParser {
     isPattern: boolean,
     refExpressionErrors?: ?ExpressionErrors,
   ): N.ObjectMember | N.SpreadElement | N.RestElement {
-    let decorators = [];
-    if (this.match(tt.at)) {
-      if (this.hasPlugin("decorators")) {
-        this.raise(this.state.start, Errors.UnsupportedPropertyDecorator);
-      }
   
-      // we needn't check if decorators (stage 0) plugin is enabled since it's checked by
-      // the call to this.parseDecorator
-      while (this.match(tt.at)) {
-        decorators.push(this.parseDecorator());
-      }
-    }
+    //.......
   
-    const prop = this.startNode();
-    let isGenerator = false;
-    let isAsync = false;
-    let startPos;
-    let startLoc;
-  
-    if (this.match(tt.ellipsis) %\colorbox{highlight}{|| this.match(tt.ellipsisAt)}%) {
+    if (this.match(tt.ellipsis) %\colorbox{highlight}{|| this.match(tt.ellipsisHash)}%) {
       if (decorators.length) this.unexpected();
       if (isPattern) {
         this.next();
@@ -36,48 +20,10 @@ export default class ExpressionParser extends LValParser {
         return this.finishNode(prop, "RestElement");
       }
   
-      return this.parseSpread(%\colorbox{highlight}{this.match(tt.ellipsisAt)}%);
+      return this.parseSpread(%\colorbox{highlight}{this.match(tt.ellipsisHash)}%);
     }
   
-    if (decorators.length) {
-      prop.decorators = decorators;
-      decorators = [];
-    }
-  
-    prop.method = false;
-  
-    if (isPattern || refExpressionErrors) {
-      startPos = this.state.start;
-      startLoc = this.state.startLoc;
-    }
-  
-    if (!isPattern) {
-      isGenerator = this.eat(tt.star);
-    }
-  
-    const containsEsc = this.state.containsEsc;
-    this.parsePropertyName(prop, /* isPrivateNameAllowed */ false);
-  
-    if (!isPattern && !containsEsc && !isGenerator && this.isAsyncProp(prop)) {
-      isAsync = true;
-      isGenerator = this.eat(tt.star);
-      this.parsePropertyName(prop, /* isPrivateNameAllowed */ false);
-    } else {
-      isAsync = false;
-    }
-  
-    this.parseObjPropValue(
-      prop,
-      startPos,
-      startLoc,
-      isGenerator,
-      isAsync,
-      isPattern,
-      refExpressionErrors,
-      containsEsc,
-    );
-  
-    return prop;
+    //.......
   }
   
   parseExprListItem(
@@ -89,31 +35,16 @@ export default class ExpressionParser extends LValParser {
     let elt;
     if (allowEmpty && this.match(tt.comma)) {
       elt = null;
-    } else if (this.match(tt.ellipsis) %\colorbox{highlight}{|| this.match(tt.ellipsisAt)}%) {
+    } else if (this.match(tt.ellipsis) %\colorbox{highlight}{|| this.match(tt.ellipsisHash)}%) {
       const spreadNodeStartPos = this.state.start;
       const spreadNodeStartLoc = this.state.startLoc;
       elt = this.parseParenItem(
-        this.parseSpread(%\colorbox{highlight}{this.match(tt.ellipsisAt)}%, refExpressionErrors, refNeedsArrowPos),
+        this.parseSpread(%\colorbox{highlight}{this.match(tt.ellipsisHash)}%, refExpressionErrors, refNeedsArrowPos),
         spreadNodeStartPos,
         spreadNodeStartLoc,
       );
-    } else if (this.match(tt.question)) {
-      this.expectPlugin("partialApplication");
-      if (!allowPlaceholder) {
-        this.raise(this.state.start, Errors.UnexpectedArgumentPlaceholder);
-      }
-      const node = this.startNode();
-      this.next();
-      elt = this.finishNode(node, "ArgumentPlaceholder");
-    } else {
-      elt = this.parseMaybeAssign(
-        false,
-        refExpressionErrors,
-        this.parseParenItem,
-        refNeedsArrowPos,
-      );
     }
-    return elt;
+    // ........
   }
 }
 
